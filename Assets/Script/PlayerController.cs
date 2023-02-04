ebc33357377b92a28f196b88c320e727d46e7033
Chase Chen <284276196@qq.com>
2023-02-04 12:15:59 +0800

    版本上传

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -1,3 +1,27 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:1a8d233f82f31a44fe895560b268328394d371f467382215dcffe24db70ea2dd
-size 647
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float runSpeed;
+    private Rigidbody2D myRigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Run();
+    }
+
+    void Run()
+    {
+        float moveDir = Input.GetAxis("Horizontal");
+        Vector2 playerVel = new Vector2(moveDir * runSpeed, myRigidbody.velocity.y);
+        myRigidbody.velocity = playerVel;
+    }
+}
